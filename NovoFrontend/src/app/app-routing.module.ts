69a36432be45482f4b4ced7da66e9583993110ac
Matheus Carvalho <matheuscarvalho@gmail.com>
2024-01-29 15:54:32 -0300

    Renomeando rotas dos arquivos

--- NovoFrontend/src/app/app-routing.module.ts
+++ NovoFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './guards/auth-guard.guard';
 import { AuthGuardChild } from './guards/auth-guard-child.guard';
 import { LogoDecisaoComponent } from './shared/components/logo-decisao/logo-decisao.component';
@@ -11,9 +11,9 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'base-cabecalho-login-register', component: LogoDecisaoComponent },
 
-  { path: '', loadChildren: () => import('./components/welcome/welcome.module').then((m) => m.WelcomeModule), canActivate: [AuthGuard] },
+  { path: '', loadChildren: () => import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule), canActivate: [AuthGuard] },
 
-  { path: 'clients', loadChildren: () => import('./components/clients/clients.module').then((m) => m.ClientsModule), canActivate: [AuthGuard], canActivateChild: [AuthGuardChild] },
+  { path: 'clients', loadChildren: () => import('./pages/clients/clients.module').then((m) => m.ClientsModule), canActivate: [AuthGuard], canActivateChild: [AuthGuardChild] },
 ];
 
 @NgModule({
